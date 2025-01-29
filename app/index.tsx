cc84f8c5dea6e859e3e3b62acd27cebad7dbe8d1
Charan <charanxmn@gmail.com>
2025-01-30 00:09:36 +0530

    Add navigation to About screen and link from Home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
@@ -6,6 +7,7 @@ export default function Index() {
       style={styles.container}
     >
       <Text style={styles.text}>Home Screen</Text>
+      <Link href="/about" style={styles.button}>Go to About</Link>
     </View>
   );
 }
@@ -20,4 +22,9 @@ const styles = StyleSheet.create({
   text: {
     color: '#fff',
   },
+  button: {
+    fontSize: 20,
+    color: '#fff',
+    textDecorationLine: 'underline',
+  }
 });
